51ef9d75822ce346c735baa4215cdbae1a6fec4f
MikeUaaIsc <ing.miguel.o.e@gmail.com>
2024-05-14 14:08:52 -0600

    Hasta aqui busqueda general, por isbn e inserción de documentos

--- router/Libros.js
+++ router/Libros.js
@@ -12,4 +12,31 @@ router.get('/', async (req, res) => {
         console.log(error)
     }
 })
-module.exports=router;
+
+// Para obtener un solo libro de la base de datos con referencia en el isbn
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const arrayLibrosDB = await Libro.find({ isbn: id });
+        console.log(arrayLibrosDB);
+        res.json(arrayLibrosDB);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Para insertar un documento en la base de datos de Mongo
+router.post('/', async (req, res) => {
+    const body = req.body;
+    try {
+        await Libro.create(body);
+        res.json({ estado: 'Libro insertado correctamente en la base de datos'});
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+
+module.exports = router;
